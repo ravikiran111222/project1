6a68c6ac61d25a7104d29448e9162c0484045bd0
ravikiran111222 <rk8500.rk@gmail.com>
2023-02-06 13:28:54 +0530

    this is my second code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import mysql.connector as conn
 
 mydb=conn.connect(host="localhost", user ="root", passwd ="mysql")
-print(mydb)
-print("connection established")
 cursor=mydb.cursor()
-cursor.execute("show databases")
+
+
+cursor.execute("select * from ravi.emp_details where city='Tumkur'")
+
+for i in cursor.fetchall():
+    print (i)
